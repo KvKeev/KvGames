185899ed6107c80e7e8821421c5f18ab1399da7f
Kevin <briankevinbotta@hotmail.com>
2025-08-05 19:01:15 -0300

    UserGameData

--- KvGames/Models/User.cs
+++ KvGames/Models/User.cs
@@ -12,17 +12,28 @@ namespace KvGames.Models
         public int Id { get; set; } = 0;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public DateTime RegistrationDate { get; set; } = DateTime.Now;
-
         public bool IsAdmin { get; set; } = false;
 
-        public User(int id, string username, string password, bool isAdmin = false)
+        public List <UserGameData> UserGameData { get; set; } = new List<UserGameData>();
+
+        public User() { }
+
+        public User(int id, string username, string password, string email, bool isAdmin = false)
         {
             Id = id;
             Username = username;
             Password = password;
-            IsAdmin = isAdmin;
+            Email = email;
         }
+
+        public bool HasDownloadedGame(int gameId)
+        {
+            return UserGameData.Any(ugd => ugd.GameId == gameId);
+        }
+
+
 
     }
 }
---
+++ KvGames/Models/UserGameData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvGames.Models
+{
+    public class UserGameData
+    {
+        public int GameId { get; set; } = 0;
+        public bool isCompleted { get; set; } = false;
+        public int RatingGiven { get; set; } = 0;
+
+    }
+}
