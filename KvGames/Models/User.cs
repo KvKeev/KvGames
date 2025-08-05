320cc618af8477122cc941d3ce964cc5d2821cdb
Kevin <briankevinbotta@hotmail.com>
2025-08-05 17:56:09 -0300

    Class User

---
+++ KvGames/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KvGames.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; } = 0;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime RegistrationDate { get; set; } = DateTime.Now;
+
+        public bool IsAdmin { get; set; } = false;
+
+        public User(int id, string username, string password, bool isAdmin = false)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            IsAdmin = isAdmin;
+        }
+
+    }
+}
